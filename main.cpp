133a611f886dde6f4f0bca8c9c44c83d658f092e
d-madeira <dominic.madeira@mths.ca>
2025-02-07 10:05:29 -0500

    pushed changes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
+// Author:      Dominic M.
+// Created:     2025-02-07
+// Description: This is the blinker program.
+
+
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-  // Repeat until program stopped
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+    // Repeat until program stopped
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000); // Wait for 1000 millisecond(s)
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000); // Wait for 1000 millisecond(s)
 }
