cd02e432874a0d3c58348941055683d26ee31ed9
Dominic Madeira <124389519+Dominic-Madeira@users.noreply.github.com>
2025-03-05 09:53:49 -0500

    done 2-04

--- 2-04/main.c
+++ 2-04/main.c
@@ -1,7 +1,7 @@
 /*
-	Author:      Dominic M.
-	Created:     2025-02-28
-	Description: This changes the color of an RGB LED continuously
+  Author:      Dominic M.
+  Created:     2025-02-28
+  Description: This changes the color of an RGB LED continuously
 */
 
 const int PIN_11 = 11;
@@ -19,19 +19,44 @@ void setup()
 void loop()
 {
     // Repeat until program stopped
-  	// Turn LED Green
+    // Turn LED Green
     digitalWrite(PIN_11, HIGH);
     digitalWrite(PIN_12, LOW);
     digitalWrite(PIN_13, LOW);
     delay(blinkTime); // Wait for 1000 millisecond(s)
-  	// Turn LED Blue
+    // Turn LED Blue
     digitalWrite(PIN_11, LOW);
     digitalWrite(PIN_12, HIGH);
     digitalWrite(PIN_13, LOW);
-  	delay(blinkTime); // Wait for 1000 millisecond(s)
-  	// Turn LED Red
+    delay(blinkTime); // Wait for 1000 millisecond(s)
+    // Turn LED Red
     digitalWrite(PIN_11, LOW);
     digitalWrite(PIN_12, LOW);
     digitalWrite(PIN_13, HIGH);
+    delay(blinkTime); // Wait for 1000 millisecond(s)
+    // Turn LED Yellow
+    digitalWrite(PIN_11, HIGH);
+    digitalWrite(PIN_12, LOW);
+    digitalWrite(PIN_13, HIGH);
+    delay(blinkTime); // Wait for 1000 millisecond(s)
+    // Turn LED Purple
+    digitalWrite(PIN_11, LOW);
+    digitalWrite(PIN_12, HIGH);
+    digitalWrite(PIN_13, HIGH);
+    delay(blinkTime); // Wait for 1000 millisecond(s)
+    // Turn LED Cyan
+    digitalWrite(PIN_11, HIGH);
+    digitalWrite(PIN_12, HIGH);
+    digitalWrite(PIN_13, LOW);
+    delay(blinkTime); // Wait for 1000 millisecond(s)
+    // Turn LED White
+    digitalWrite(PIN_11, HIGH);
+    digitalWrite(PIN_12, HIGH);
+    digitalWrite(PIN_13, HIGH);
+    delay(blinkTime); // Wait for 1000 millisecond(s)
+    // Turn LED Off
+    digitalWrite(PIN_11, LOW);
+    digitalWrite(PIN_12, LOW);
+    digitalWrite(PIN_13, LOW);
     delay(blinkTime); // Wait for 1000 millisecond(s)
 }
